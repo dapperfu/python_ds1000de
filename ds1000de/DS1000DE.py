68d78ab3cba43c3d6fece9f3c8cdf63cd804562e
Frey, Jed <jed-frey@users.noreply.github.com>
2017-09-26 13:17:44 -0400

    Unit and style testing and cleanup

--- ds1000de/DS1000DE.py
+++ ds1000de/DS1000DE.py
@@ -1,17 +1,59 @@
 # -*- coding: utf-8 -*-
+"""Python module for Rigol DS1000DE series oscilloscopes."""
+
 import visa
 
+
 class DS1000DE(object):
-    def __init__(self, device="USB0"):
-        rm = visa.ResourceManager("@py")
+    """Python class for Rigol DS1000DE series oscilloscopes."""
+
+    def __init__(self, device="USB"):
+        """DS1000DE Init."""
+        try:
+            rm = visa.ResourceManager()
+        except BaseException:
+            rm = visa.ResourceManager("@py")
         resources = rm.list_resources()
-        
+
         resource = [resource for resource in resources if device in resource]
         if len(resource) == 0:
             raise Exception("No devices found matching '{}'".format(device))
         if len(resource) > 1:
-            raise Exception("Multiple devices found matching '{}':\n\t{}".format(device, "\n\t".join(resource)))
-        
+            raise Exception(
+                "Multiple devices found matching '{}':\n\t{}".format(
+                    device, "\n\t".join(resource)))
+
         self.resource = resource[0]
-        
+
         self.inst = rm.open_resource(self.resource)
+        self.r = self.inst.query
+        self.w = self.inst.write
+
+    @property
+    def idn(self):
+        """Return *IDN?."""
+        return self.r("*IDN?")
+
+    @property
+    def manufacturer(self):
+        """Return manufacturer."""
+        manufacturer, model, serial, software = self.idn.split(",")
+        return manufacturer
+
+    @property
+    def model(self):
+        """Return model."""
+        manufacturer, model, serial, software = self.idn.split(",")
+        return model
+
+    @property
+    def serial(self):
+        """Return serial."""
+        manufacturer, model, serial, software = self.idn.split(",")
+        return serial
+
+    @property
+    def software(self):
+        """Return software version."""
+        manufacturer, model, serial, software = self.idn.split(",")
+        return software
--- setup.py
+++ setup.py
@@ -1,21 +1,30 @@
 # -*- coding: utf-8 -*-
+"""Python module for Rigol DS1000DE series oscilloscopes."""
+
 from setuptools import setup
 
-setup(name='ds1000de',
-      version='0.1',
-      description='Python module for controlling Rigol DS1000D/E through PyVISA',
-      url='https://github.com/jed-frey/python-ds1000de',
-      author='Jed Frey',
-      license='BSD',
-      packages=['ds1000de'],
-      setup_requires=[
-              'pytest-runner',
-              'pyvisa',
-              ],
-      tests_require=[
-              'pytest',
-              'pytest-html',
-              'pytest-flake8',
-              ],
-      zip_safe=False,
-      )
+setup(
+    name='ds1000de',
+    version='0.1',
+    description='Python module for controlling Rigol DS1000D/E through PyVISA',
+    url='https://github.com/jed-frey/python-ds1000de',
+    author='Jed Frey',
+    license='BSD',
+    packages=['ds1000de'],
+    setup_requires=[
+        'pytest-runner',
+        'pyvisa',
+    ],
+    tests_require=[
+        'pytest',
+        'pytest-html',
+        'pytest-flake8',
+        'flake8',
+        'flake8-isort',
+        'flake8-junit-report',
+        'flake8-docstrings',
+        'autopep8',
+        'isort',
+    ],
+    zip_safe=False,
+)
